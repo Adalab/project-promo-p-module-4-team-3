0c73736e4face70155aecba6a536c8afe05b2bab
celiarodriguezmo <celiro02@gmail.com>
2022-05-23 21:22:22 +0200

    footer name

--- web/src/components/Footer.js
+++ web/src/components/Footer.js
@@ -3,7 +3,7 @@ import logoAdalab from "../images/logo-adalab.png";
 const Footer = () => {
   return (
     <footer className='footer'>
-      <p className='footer__paragraph footer-paragraph'>Sine Nomine © 2022</p>
+      <p className='footer__paragraph footer-paragraph'>Star Coders © 2022</p>
       <a href='https://adalab.es' target='_blank' rel='noreferrer'>
         <img className='footer__img' src={logoAdalab} alt='Logo Adalab' />
       </a>
